60eaccbe12b49af6897cb9891b4aa50e47dd81c8
cesarsantiagonunez <86664126+cesarsantiagonunez@users.noreply.github.com>
2022-11-03 20:57:54 +0100

    Update ex1_solution.sh
    exercise 1 commands

--- lesson_3/ex1_solution.sh
+++ lesson_3/ex1_solution.sh
@@ -1,1 +1,7 @@
 docker pull
+docker run -d -e POSTGRES_PASSWORD=abc123. postgres:latest
+docker image ls postgres:latest
+docker ps -a
+docker inspect --format="{{json .Config.ExposedPorts }}" postgres:latest
+docker inspect --format="{{json .Config.Env }}" postgres:latest
+docker stats naughty_faraday
