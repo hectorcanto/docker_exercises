445f1f1bda9ca882521da35201336f20f7c98b46
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2022-11-03 23:12:08 +0100

    Update ex2_solution.sh

--- lesson_3/ex2_solution.sh
+++ lesson_3/ex2_solution.sh
@@ -1,3 +1,5 @@
 tags=('nginx:1.21.1' 'nginx:1.21.1-alpine' 'nginx:1.21.1-alpine-perl' 'nginx:1.21.1-perl')
 
 for tag in ${tags[@]}; do docker pull "$tag"; done
+
+docker images nginx --format "{{.Repository}} | {{.Tag}} | {{.ID}} | {{.CreatedSince}} | {{.Size}}"
