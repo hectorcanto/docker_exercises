0e0b842ced0e1bc68cb82ee2ab951975e64603ce
alberto-goitia-llovo <goitia.alberto.ll@gmail.com>
2022-11-03 21:00:29 +0100

    exercise 1 completed

--- lesson_3/ex1_solution.sh
+++ lesson_3/ex1_solution.sh
@@ -1,1 +1,5 @@
-docker pull
+docker pull postgres:latest
+docker image ls
+docker inspect postgres:latest
+docker run -d -e POSTGRES_PASSWORD='password' postgres:latest
+docker exec -it 651d95b481aa bash
