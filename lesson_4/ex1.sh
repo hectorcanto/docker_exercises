6e064f265c764b3763ca85bdd3e1d9a71684f15c
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2022-11-09 22:07:54 +0100

    Update ex1.sh

--- lesson_4/ex1.sh
+++ lesson_4/ex1.sh
@@ -2,35 +2,17 @@ docker pull nginx:1.21.1
 
 docker run --name my-nginx -d -p 8011:80 nginx:1.21.1
 
-docker stop my-nginx && docker container rm my-nginx
+docker ps ## I want to check the current port mapping
+
+docker stop my-nginx 
+
+docker rm my-nginx
 
 docker run --name my-nginx -d -p 8888:80 -v $(pwd)/docker_course/lesson04/nginx/config/ex1.conf:/etc/nginx/nginx.conf -v $(pwd)/docker_course/lesson04/nginx/view/ex1_web.html:/var/www/index.html nginx:1.21.1
 
+docker logs my-nginx
 
-### something was wrong
+docker stop my-nginx
 
-docker exec -it my-nginx sh
-
-cat /var/www/index.html
-
-<!DOCTYPE html>
-<title>Text Example</title>
-<style>
-div.container {
-background-color: #ffffff;
-}
-div.container p {
-font-family: Arial;
-font-size: 14px;
-font-style: normal;
-font-weight: normal;
-text-decoration: none;
-text-transform: none;
-color: #000000;
-background-color: #ffffff;
-}
-</style>
-
-<div class="container">
-<p>Docker course</p>
-</div>#
+docker inspect my-nginx | jq '.[0] | [.Name, .State.Status, .State.ExitCode ]'
+ 
