e161da4086dfacec17946ec89495198d48534173
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2022-11-09 22:34:26 +0100

    Update ex1_solution.sh

--- lesson_3/ex1_solution.sh
+++ lesson_3/ex1_solution.sh
@@ -1,11 +1,1 @@
-
- docker run --name some-postgres -e POSTGRES_PASSWORD=mypassword -d postgres
-
- docker images
-
- docker ps
-
- docker inspect postgres:latest | jq -C | less -R
-
- docker stats --no-stream some-postgres
-
+docker pull
