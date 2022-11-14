40e6c0e1b135bc0a0f957d0b01f961c34ae83de7
alberto-goitia-llovo <goitia.alberto.ll@gmail.com>
2022-11-14 21:42:31 +0100

    ex2 completed

--- lesson_7_exercises/ex2.sh
+++ lesson_7_exercises/ex2.sh
@@ -1,1 +1,5 @@
-docker-compose ...
+docker-compose -f ex2.dkc.yml up -d
+docker run mariadb:latest # Error detected > env variables needed > environment added to .yml
+docker-compose -f ex2.dkc.yml up -d
+docker exec -it lesson_7_exercises_db_1 mysql -uroot -proot -e "CREATE DATABASE lesson7"
+docker exec -it lesson_7_exercises_db_1 mysql -uroot -proot -e "show databases"
