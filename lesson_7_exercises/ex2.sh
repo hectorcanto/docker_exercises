8e6bc1152587712917bca0b087280d0bb22f0793
Julio Lagos (julio.lagos) <julio.lagos@imatia.com>
2022-11-16 07:07:36 +0100

    Exercise 1 and 2 solved

--- lesson_7_exercises/ex2.sh
+++ lesson_7_exercises/ex2.sh
@@ -1,1 +1,5 @@
-docker-compose ...
+docker-compose -f ex2.dkc.yml up -d
+
+docker-compose -f ex2.dkc.yml exec ex2-mariadb mysql --user=root --password=m4r14db.r00t -e "SHOW DATABASES;"
+docker-compose -f ex2.dkc.yml exec ex2-mariadb mysql --user=root --password=m4r14db.r00t -e "CREATE DATABASE lesson7_ex2;"
+docker-compose -f ex2.dkc.yml exec ex2-mariadb mysql --user=root --password=m4r14db.r00t -e "SHOW DATABASES;"
